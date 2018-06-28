2fa25871555062ae4e7de361018abbe7fe9b664a
Shota Fubasami <shotafubasami@iMac.local>
2018-06-28 22:07:46 +0900

    Add ! mark in fizzbuzz

--- test_branch/fizzbuzz.py
+++ test_branch/fizzbuzz.py
@@ -3,10 +3,10 @@
 N = 100
 for i in range(1,N):
     if i%15 == 0:
-        print('Fizz Buzz')
+        print('Fizz! Buzz!')
     elif i%5 == 0:
-        print('Buzz')
+        print('Buzz!')
     elif i%3 == 0:
-        print('Fizz')
+        print('Fizz!')
     else:
         print(i)
